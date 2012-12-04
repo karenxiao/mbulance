325569466af219ea2765be570ef6e0674ba5806a
Harvard China Care <care@cato.hcs.harvard.edu>
2012-12-04 11:46:10 -0500

    adding trees

--- index.php
+++ index.php
@@ -1,9 +1,51 @@
 <?php
 
+session_start();
+
 require_once("response.php");
+require_once("trees.php");
 
 $r = new Response();
-$r->setFiller("yes");
-$r->addPlayAudio("http://mbulance.karenxiao.com/audio/test.wav");
+
+// code to look up the specific string in the tree
+$_SESSION['code'] = "";
+
+if($_REQUEST['event']=="NewCall") 
+  {
+    $cd = new CollectDtmf();
+    $cd->setTimeOut("4000");
+    $cd->addPlayText("Welcome to Em Bulance mobile diagnostic system. To begin, press 1. To respond yes to a question, press 1. To respond no to a question press 2.");
+    $r->addCollectDtmf($cd);
+    $r->send();
+  } 
+
+while($_REQUEST['event']=="GotDTMF")
+   {
+
+     $response = $_REQUEST['data'];
+
+     if ($response == 1)
+       {
+	 $cd = new CollectDtmf();
+	 $cd->setTimeOut("4000");
+	 $code = $_SESSION['code'] . '1';
+	 $cd->addPlayText($code);
+	 $r->addCollectDtmf($cd);
+	 $r->send();
+       }
+     else if ($response == 2)
+       {
+	 $cd = new CollectDtmf();
+	 $cd->setTimeOut("4000");
+	 $code = $_SESSION['code'] . '0';
+	 $cd->addPlayText($code);
+	 $r->addCollectDtmf($cd);
+	 $r->send(); 
+       }
+
+   } 
+
+$r->addHangup();
+$r->send();
 
 ?>
