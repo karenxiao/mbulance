97507ed4bfcce7a70083ccdaa3f82cc4e3293773
Karen Xiao <karenxiao240@gmail.com>
2013-04-18 20:42:44 -0400

    testing punjabi audio files

--- index1.php
+++ index1.php
@@ -8,53 +8,53 @@ session_start();
 $r = new Response();
 
 if ($_REQUEST['event']=="NewCall") 
+{
+  $cd = new CollectDtmf();
+  $cd->setMaxDigits("1");
+  $cd->setTimeOut("4000");
+  $cd->addPlayText("Welcome to em bulance mobile healthcare diagnostic service. Press one to begin.");
+
+  $_SESSION['code'] = "";
+
+  $r->addCollectDtmf($cd);
+  $r->send();
+} 
+
+else if ($_REQUEST['event']=="GotDTMF")
+{
+  $response = $_REQUEST['data'];
+
+  if ($response == 1 || $response == 0)
   {
+    $_SESSION['code'] .= $response;
+    $string = "http://karenxiao.com/mbulance/mbulance/audio/cold/e" . $_SESSION['code'] . ".wav";
+
     $cd = new CollectDtmf();
     $cd->setMaxDigits("1");
     $cd->setTimeOut("4000");
-    $cd->addPlayText("Welcome to em bulance mobile healthcare diagnostic service. Press one to begin.");
+    $cd->addPlayAudio($string);
 
-    $_SESSION['code'] = "";
-    
     $r->addCollectDtmf($cd);
     $r->send();
-  } 
-
-else if ($_REQUEST['event']=="GotDTMF")
-  {
-     $response = $_REQUEST['data'];
-     
-     if ($response == 1 || $response == 0)
-       {
-   $_SESSION['code'] .= $response;
-   $string = $cold[$_SESSION['code']];
-
-   $cd = new CollectDtmf();
-   $cd->setMaxDigits("1");
-   $cd->setTimeOut("4000");
-   $cd->addPlayText($string);
-
-   $r->addCollectDtmf($cd);
-   $r->send();
-       }
-
-     else if ($response == 2)
-       {
-   $r->addPlayText("Thank you for using em bulance. Goodbye!");
-   $r->addHangup();
-   $r->send();
-       }
-
-     else
-       {
-   $r->addPlayText("Sorry. That is not a valid response. Please press one for yes, zero for no.");
-   $r->send();
-       }
-   } 
-else 
-  {
-     $r->addPlayText("Sorry. The system has timed out. Goodbye!");
-     $r->addHangup();
-     $r->send();
   }
+
+  else if ($response == 2)
+  {
+    $r->addPlayText("Thank you for using em bulance. Goodbye!");
+    $r->addHangup();
+    $r->send();
+  }
+
+  else
+  {
+    $r->addPlayText("Sorry. That is not a valid response. Please press one for yes, zero for no.");
+    $r->send();
+  }
+} 
+else 
+{
+  $r->addPlayText("Sorry. The system has timed out. Goodbye!");
+  $r->addHangup();
+  $r->send();
+}
 ?>
