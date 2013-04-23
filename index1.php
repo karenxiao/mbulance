7701e194565ec1609f9b69c10cb74009e21b7d17
Karen Xiao <karenxiao240@gmail.com>
2013-04-23 15:32:23 -0400

    cleaning up files

--- index1.php
+++ index1.php
@@ -6,45 +6,45 @@ require_once("trees.php");
 $r = new Response();
 
 if ($_REQUEST['event']=="NewCall") 
+{
+  $cd = new CollectDtmf();
+  $cd->setMaxDigits("1");
+  $cd->setTimeOut("4000");
+  $cd->addPlayText("Welcome to em bulance mobile healthcare diagnostic service. Press one to begin.");
+
+  $_SESSION['code'] = "";
+
+  $r->addCollectDtmf($cd);
+  $r->send();
+} 
+
+else if ($_REQUEST['event']=="GotDTMF")
+{
+  $response = $_REQUEST['data'];
+
+  if ($response == 1 || $response == 0)
   {
+    $_SESSION['code'] .= $response;
+    $string = $cold[$_SESSION['code']];
+
     $cd = new CollectDtmf();
     $cd->setMaxDigits("1");
     $cd->setTimeOut("4000");
-    $cd->addPlayText("Welcome to em bulance mobile healthcare diagnostic service. Press one to begin.");
-
-    $_SESSION['code'] = "";
+    $cd->addPlayText($string);
 
     $r->addCollectDtmf($cd);
     $r->send();
-  } 
-
-else if ($_REQUEST['event']=="GotDTMF")
-  {
-     $response = $_REQUEST['data'];
-     
-     if ($response == 1 || $response == 0)
-       {
-   $code = $_SESSION['code'] . $response;
-   $string = $cold[$code];
-
-   $cd = new CollectDtmf();
-   $cd->setMaxDigits("1");
-   $cd->setTimeOut("4000");
-   $cd->addPlayText($string);
-
-   $r->addCollectDtmf($cd);
-   $r->send();
-       }
-     else
-       {
-   $r->addPlayText("Sorry. That is not a valid response. Please press one for yes, two for no.");
-   $r->send();
-       }
-   } 
-else 
-  {
-     $r->addPlayText("Goodbye!");
-     $r->addHangup();
-     $r->send();
   }
+  else
+  {
+    $r->addPlayText("Sorry. That is not a valid response. Please press one for yes, two for no.");
+    $r->send();
+  }
+} 
+else 
+{
+  $r->addPlayText("Goodbye!");
+  $r->addHangup();
+  $r->send();
+}
 ?>
